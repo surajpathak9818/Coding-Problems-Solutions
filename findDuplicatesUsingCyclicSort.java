d247cad8f445b1af6b5e31d3ccb5eb613e4341cc
Suraj Pathak <surajpathak9818@gmail.com>
2025-08-27 13:21:24 +0530

    Find the Duplicates number in the array list
    this is code for finding the duplicates numbers in the array using the cyclic sort algorithm

---
+++ findDuplicatesUsingCyclicSort.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class findDuplicatesUsingCyclicSort {
+
+    public List<Integer> findDuplicates(int[] arr) {
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for(int index = 0; index < arr.length; index++){
+            if(arr[index] != index + 1){
+                ans.add(arr[index]);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    // Add this main method to run your class
+    public static void main(String[] args) {
+        findDuplicatesUsingCyclicSort obj = new findDuplicatesUsingCyclicSort();
+        int[] arr = {4,3,2,7,8,2,9,9,3,1};
+        System.out.println(obj.findDuplicates(arr));
+    }
+}
